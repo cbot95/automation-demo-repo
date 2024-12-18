d2649844ab0b042fac4a2aa4c6ef3776ef2174ac
cbot95 <chloe.talbot@gmail.com>
2024-12-18 23:50:42 +0900

    add error test for incorrect credentials

--- cypress/integration/sauceDemo/login-error.spec.js
+++ cypress/integration/sauceDemo/login-error.spec.js
@@ -1,3 +1,4 @@
+import { describe } from 'node:test';
 import sauceDemo from '../../pageObjects/sauceDemo.json'
 const {login, credentials} = sauceDemo
 
@@ -6,37 +7,52 @@ const  assertNoErrorElements = () => {
     cy.get(login.fieldErrorIcon).should('not.exist')
 }
 
+const errorIconsVisible = () => {
+    cy.get(login.fieldErrorIcon).should('have.length', 2).and('be.visible')
+}
+ 
 beforeEach(() => {
     cy.goToPage()
     assertNoErrorElements()
 });
 
 
-describe('Login error Tests', () => {
+describe('Tests login forms when fields are incomplete', () => {
     it('should display error message when all login fields are incomplete', () => {
-        // TODO:  within login container icon should not exist in both field
-        cy.assertVisibility(login.errorBanner, 'not.exist');
         cy.clickDataTest(login.button)
         cy.assertVisibility(login.errorBanner).and('have.text', 'Epic sadface: Username is required');
-        cy.get(login.fieldErrorIcon).should('have.length', 2).and('be.visible')
+        errorIconsVisible()
     })
     it('should display error message when password field is incomplete', () => {
-        cy.assertVisibility(login.errorBanner, 'not.exist'
-        );
         cy.getdataTest(login.usernameField).type(credentials.standardUser);
         cy.clickDataTest(login.button)
+        errorIconsVisible()
         cy.assertVisibility(login.errorBanner).and('have.text', 'Epic sadface: Password is required');
-        cy.get(login.formField).eq(1).find(login.fieldErrorIcon).should('be.visible') //asserts icon is visible in the password field
     })
     it('should display error message when email field is incomplete', () => {
-        cy.assertVisibility(login.errorBanner, 'not.exist'
-        );
         cy.getdataTest(login.passwordField).type(credentials.password);
         cy.clickDataTest(login.button)
         cy.assertVisibility(login.errorBanner).and('have.text', 'Epic sadface: Username is required');
-        cy.get(login.formField).eq(0).find(login.fieldErrorIcon).should('be.visible') //asserts icon is visible in the username field
+        errorIconsVisible()
     })
+    
+});
 
-  });
+describe('Tests login form when user credentials are incorrect', () =>{
+    it('should display error message when password is incorrect', () => {
+        cy.login(credentials.standardUser, 'incorrectpwd')
+        cy.clickDataTest(login.button)
+        cy.assertVisibility(login.errorBanner).and('have.text', 'Epic sadface: Username and password do not match any user in this service');
+        errorIconsVisible();
+    })
+    it('should display error message when user is incorrect', () => {
+        cy.login('incorrectuser', credentials.password)
+        cy.clickDataTest(login.button)
+        cy.assertVisibility(login.errorBanner).and('have.text', 'Epic sadface: Username and password do not match any user in this service');
+        errorIconsVisible();
+    })
+  })
+
+
 
   
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import sauceDemo from '../pageObjects/sauceDemo.json'
+const {login} = sauceDemo
 
 
 Cypress.Commands.add('assertVisibility', (dataTest, visibility = 'be.visible') => {
@@ -37,9 +14,10 @@ Cypress.Commands.add('clickDataTest', (dataTest, options = {}) => {
 Cypress.Commands.add('getdataTest', (dataTest) => {
     return cy.get(`[data-test=${dataTest}]`);
 })
+
 // Added the following command to ensure the session gets cleared after each run. 
 Cypress.Commands.add('goToPage', (pagePath = '') => {
-    // Clear session storage
+
     cy.window().then((win) => {
       win.sessionStorage.clear();
     });
@@ -48,4 +26,8 @@ Cypress.Commands.add('goToPage', (pagePath = '') => {
     cy.visit(pagePath);
   });
   
+Cypress.Commands.add('login', (username, password) => {
+    cy.getdataTest(login.usernameField).type(username);
+    cy.getdataTest(login.passwordField).type(password);
+})
   
